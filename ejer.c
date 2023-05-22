a49bea8621804efdc0d47654f233a5c584ca94fe
Sol8888 <131205430+Sol8888@users.noreply.github.com>
2023-05-22 16:22:47 -0500

    tercer commint
    Programa ya comentado

--- ejer.c
+++ ejer.c
@@ -3,37 +3,38 @@
 #define M 20
 #define N 20
 
-int main ()
+int main()
 {
     int filas, columnas, i, j, A[M][N], T[M][N];
 
-    printf ("escribe numero de filas: ");
-    scanf ("%d", &filas);
-    printf ("escribe numero de columnas: ");
-    scanf ("%d", &columnas);
+    printf("Escribe el número de filas: ");
+    scanf("%d", &filas);
+    printf("Escribe el número de columnas: ");
+    scanf("%d", &columnas);
 
-    for ( i = 0; i < filas; i++)
+    // Llenar la matriz A con valores aleatorios y mostrarla
+    for (i = 0; i < filas; i++)
     {
-        for ( j = 0; j < columnas; j++)
+        for (j = 0; j < columnas; j++)
         {
-           A[i][j] = rand() % 101;  
-           printf ("%d\t", A[i][j]);
+            A[i][j] = rand() % 101;  // Asignar un valor aleatorio entre 0 y 100
+            printf("%d\t", A[i][j]);
         }
         printf("\n");
     }
 
-    printf("segunda matriz\n");
+    printf("Matriz transpuesta:\n");
 
-    for ( i = 0; i < filas; i++)
+    // Calcular y mostrar la matriz transpuesta T
+    for (i = 0; i < filas; i++)
     {
-        for ( j = 0; j < columnas; j++)
+        for (j = 0; j < columnas; j++)
         {
-           T[i][j] = A[j][i];
-           printf ("%d\t", T[i][j]);
+            T[i][j] = A[j][i];  // Transponer los elementos de A en T
+            printf("%d\t", T[i][j]);
         }
         printf("\n");
     }
 
     return 0;
-
 }
